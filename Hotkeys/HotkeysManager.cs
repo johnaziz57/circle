6497bdfbca69173671f1037aaa888e444986e0b8
John Salib <john_aziz57@yahoo.com>
2024-12-10 21:51:29 +0100

    Add project files.

---
+++ Hotkeys/HotkeysManager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Interop;
+
+namespace Circle_2.Hotkeys
+{
+    public static class HotkeysManager
+    {
+        #region WinAPI Functions
+
+        [DllImport("user32.dll")]
+        private static extern bool RegisterHotKey(IntPtr hWnd, int id, uint fsModifiers, int vk);
+
+        [DllImport("user32.dll")]
+        private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+        // Windows message type when the hot key is triggered
+        public const int WM_HOTKEY = 0x0312;
+
+        // Modifier key constants
+        public const int HOTKEY_ID = 9000; // A unique ID for the hotkey
+
+        #endregion
+
+        public static bool RegisterHotKey(
+            WindowInteropHelper helper,
+            HotKeyModifier hotKeyModifier,
+            Key key)
+        {
+            return RegisterHotKey(helper.Handle, HOTKEY_ID, (uint)hotKeyModifier, (int)KeyInterop.VirtualKeyFromKey(key));
+        }
+
+
+        public static bool UnregisterHotKey(WindowInteropHelper helper)
+        {
+            return UnregisterHotKey(helper.Handle, HOTKEY_ID);
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Circle_2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private const int HOTKEY_ID = 9000; // A unique ID for the hotkey
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += MainWindow_Loaded;
+            Closing += MainWindow_Closing;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Register the hotkey: Win + Alt + Enter
+            var helper = new WindowInteropHelper(this);
+            if (!RegisterHotKey(helper.Handle, HOTKEY_ID, MOD_WIN | MOD_ALT, (int)KeyInterop.VirtualKeyFromKey(Key.Enter)))
+            {
+                MessageBox.Show("Failed to register hotkey.");
+            }
+
+            // Hook into WPF's message loop
+            HwndSource source = HwndSource.FromHwnd(helper.Handle);
+            source.AddHook(HwndHook);
+        }
+
+        private void MainWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            // Unregister the hotkey when the application is closing
+            var helper = new WindowInteropHelper(this);
+            UnregisterHotKey(helper.Handle, HOTKEY_ID);
+        }
+
+        private IntPtr HwndHook(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            if (msg == WM_HOTKEY && wParam.ToInt32() == HOTKEY_ID)
+            {
+                // Hotkey triggered
+                MessageBox.Show("Hotkey triggered: Win + Alt + Enter");
+                handled = true; // Mark the message as handled
+            }
+
+            return IntPtr.Zero;
+        }
+
+        #region WinAPI Functions
+
+        [DllImport("user32.dll")]
+        private static extern bool RegisterHotKey(IntPtr hWnd, int id, uint fsModifiers, int vk);
+
+        [DllImport("user32.dll")]
+        private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+        // Windows message type when the hot key is triggered
+        private const int WM_HOTKEY = 0x0312;
+
+        // Modifier key constants
+        private const uint MOD_ALT = 0x0001;
+        private const uint MOD_CONTROL = 0x0002;
+        private const uint MOD_SHIFT = 0x0004;
+        private const uint MOD_WIN = 0x0008;
+
+        #endregion
+    }
+}
