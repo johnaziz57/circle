22189eedbe55c0a8d5673e095f720fe3a3c1a2dc
John Salib <john_aziz57@yahoo.com>
2024-12-15 14:55:00 +0100

    Add Trayicon

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System.Windows;
+﻿using System.Drawing;
+using System.Windows;
 using Circle_2.Logic;
+using Hardcodet.Wpf.TaskbarNotification;
 
 namespace Circle_2
 {
@@ -9,17 +11,35 @@ namespace Circle_2
     public partial class App : Application
     {
 
+        MainWindow? mainWindow = null;
+        TaskbarIcon? trayIcon = null;
         /// TODO here
         /// 
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
             (new MainLogic()).OnAppLoaded();
+
+            // Ensure TaskbarIcon is loaded
+            trayIcon = (TaskbarIcon)FindResource("TrayIcon");
+
         }
 
         protected override void OnExit(ExitEventArgs e)
         {
             base.OnExit(e);
+            trayIcon?.Dispose();
+        }
+
+        public void OpenSettings_Click(object sender, RoutedEventArgs e)
+        {
+            mainWindow = mainWindow ?? new MainWindow();
+            mainWindow.Show();
+        }
+
+        public void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Current.Shutdown();
         }
     }
 
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -21,5 +21,7 @@ namespace Circle_2
 
         public MainWindow()
         { }
+
+
     }
 }
